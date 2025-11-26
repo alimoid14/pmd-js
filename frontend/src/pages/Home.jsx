5c9363e3628282bca7bcd573c25735095c3c3c8d
alimoid14 <syedalimoidrizvi@gmail.com>
2025-11-26 19:54:55 +0530

    Initialize the project

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <section className='max-w-7xl bg-gray-200 mx-auto h-[100vh]'>
+      <h1>age</h1>
+    </section>
+  )
+}
+
+export default Home
